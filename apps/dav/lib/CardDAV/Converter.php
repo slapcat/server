e26db29231f472083b3d0df54c5af6699ae70b8c
Jake Nabasny <jake@nabasny.com>
2023-12-27 12:56:42 -0500

    Add back scope as requested by @ChristophWurst
    Signed-off-by: Jake Nabasny <jake@nabasny.com>

--- apps/dav/lib/CardDAV/Converter.php
+++ apps/dav/lib/CardDAV/Converter.php
@@ -140,9 +140,9 @@ class Converter {
 					$vCard->add($dateProperty);
 					break;
 				case IAccountManager::PROPERTY_ANNIVERSARYDATE:
-					$vCard->add(new Text($vCard, 'ANNIVERSARY', substr($property->getValue(), 0, 8), ['VALUE' => 'DATE']));
+					$vCard->add(new Text($vCard, 'ANNIVERSARY', substr($property->getValue(), 0, 8), ['VALUE' => 'DATE',  'X-NC-SCOPE' => $scope]));
 					/* iOS compatibility */
-					$vCard->add(new Text($vCard, 'ITEM1.X-ABDATE', substr($property->getValue(), 0, 8), ['TYPE' => 'pref']));
+					$vCard->add(new Text($vCard, 'ITEM1.X-ABDATE', substr($property->getValue(), 0, 8), ['TYPE' => 'pref',  'X-NC-SCOPE' => $scope]));
 					$vCard->add(new Text($vCard, 'ITEM1.X-ABLABEL:_$!<Anniversary>!$_'));
 					break;
 			}
